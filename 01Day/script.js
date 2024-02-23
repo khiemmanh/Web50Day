0815e5f757019f1b01ced005b230d0f355111c4d
khiem.manh.298 <khiem.manh.298@gmail.com>
2024-02-23 23:50:22 +0700

    JS: add new class name when click any element

--- 01Day/script.js
+++ 01Day/script.js
@@ -1,3 +1,17 @@
-const panels = document.querySelectorAll('.panel');
+const panels = document.getElementsByClassName("panel");
 
-console.log(panels[0]);
+for (let i = 0; i < panels.length; i++) {
+    // console.log(panels[i]);
+    panels[i].addEventListener("click", () => {
+        clearAllActiveClass();
+
+        // after click then need to add "active class name for document"
+        panels[i].classList.add("active");
+    } );
+}
+
+function clearAllActiveClass() {
+    for (let i = 0; i < panels.length; i++) {
+        panels[i].classList.remove("active");
+    }
+}
