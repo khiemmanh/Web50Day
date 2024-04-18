80a4d934fdbfe3be1c4a98c6e225ff32da2ee2c1
khiem.manh.298 <khiem.manh.298@gmail.com>
2024-04-18 16:34:30 +0700

    10Day_dad_jokes - use Fetch, http header response, .then(), async await

---
+++ 10Day_Dad_Jokes/script.js
@@ -0,0 +1,61 @@
+const jokesContent = document.querySelector(".jokes");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", () => {
+    // jokesContent.innerText = data.joke;        
+    const jokeJSON = fetchJokesData();
+    jokesContent.innerText = jokeJSON.joke;
+    console.log(jokeJSON);
+});
+
+btn.addEventListener("click", fetchJokesData);
+
+fetchJokesData();
+async function fetchJokesData() {
+    try {
+        const httpResponseHeader = {
+            headers: {
+                Accept: 'application/json'
+            },
+        }
+        
+        const reponse = await fetch('https://icanhazdadjoke.com', httpResponseHeader);
+        const data = await reponse.json();
+        jokesContent.innerHTML = data.joke;
+        // console.log(data);
+        
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+    
+    // function fetchJokesData() {
+        //     /*
+        //     JS_NOTE: HTTP header reponse
+        //     Content-Type: Specifies the type of data you're sending to the server. 
+        //     It's about the request body's format.
+        //     Accept: Specifies the type of data you expect back from the server. 
+        //     It's about the response's content format.
+    
+//     */
+//     const httpResponseHeader = {
+//        // headers: {
+//            //     'Content-Type': 'application/json'
+//            // },
+           
+//            headers: {
+//                Accept: 'application/json'
+//             },
+//     }
+        
+//     fetch('https://icanhazdadjoke.com', httpResponseHeader)
+//     .then((res) => res.json())
+//     .then(data => {
+//         console.log(data);
+//         return jokesContent.innerHTML = data.joke;  
+//     })
+//     .catch(error => console.error("Error", error));
+    // }
+    
